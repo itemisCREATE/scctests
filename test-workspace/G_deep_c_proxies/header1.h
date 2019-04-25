00bb0e624b06613f7ab4d19d891bf5eb37ff5537
andreas muelder <muelder@itemis.de>
2019-04-25 15:58:05 +0200

    added proxy test
    added proxy test

---
+++ test-workspace/G_deep_c_proxies/header1.h
@@ -0,0 +1,11 @@
+#ifndef HEADER1_H_
+#define HEADER1_H_
+
+#include "header2.h"
+
+typedef struct struct1
+{
+    struct2_t member;
+} struct1_t;
+
+#endif /* HEADER1_H_ */
---
+++ test-workspace/G_deep_c_proxies/header2.h
@@ -0,0 +1,9 @@
+#ifndef HEADER2_H_
+#define HEADER2_H_
+
+typedef struct struct2
+{
+    int value;
+} struct2_t;
+
+#endif /* HEADER2_H_ */
